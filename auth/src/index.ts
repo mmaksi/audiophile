3931c14a0db6e592a2b3ef5f70f12c539da09260
mmaksi <mmaksi.dev@gmail.com>
2024-02-23 00:13:27 -0800

    added mongo service and pod and connected to it from the auth microservice

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,16 @@
+import mongoose from 'mongoose';
 import { app } from './app';
 
-app.listen(3000, () => {
-  console.log(`Listening on port 3000!`);
-});
+const startServer = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error(error);
+  }
+  app.listen(3000, () => {
+    console.log(`Listening on port 3000!`);
+  });
+};
+
+startServer();
