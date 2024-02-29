2c0f611bd893e611674d3e67521200179cb5a5f7
mmaksi <mmaksi.dev@gmail.com>
2024-02-29 09:52:16 +0100

    updated the common package to be published to npm

--- common/src/index.ts
+++ common/src/index.ts
@@ -1,7 +1,11 @@
-interface Color {
-  red: number;
-}
+export * from './errors/bad-request.error';
+export * from './errors/custom-error';
+export * from './errors/database.error';
+export * from './errors/not-authorized.error';
+export * from './errors/not-found.error';
+export * from './errors/request-validation.error';
 
-const color: Color = { red: 250 };
-
-export default color;
+export * from './middleware/current-user.middleware';
+export * from './middleware/errors-handler.middleware';
+export * from './middleware/request-validator.middleware';
+export * from './middleware/require-auth.middleware';
---
+++ common/src/middleware/errors-handler.middleware.ts
@@ -0,0 +1,20 @@
+import { NextFunction, Request, Response } from 'express';
+import { CustomError } from '../errors/custom-error';
+
+function errorsHandler(
+  error: Error,
+  _: Request,
+  res: Response,
+  next: NextFunction
+) {
+  if (error instanceof CustomError) {
+    return res
+      .status(error.statusCode)
+      .json({ errors: error.serializeErrors() });
+  }
+  return res
+    .status(400)
+    .json({ errors: [{ message: 'Something went wrong' }] });
+}
+
+export default errorsHandler;
