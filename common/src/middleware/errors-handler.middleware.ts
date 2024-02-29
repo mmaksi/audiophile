0e0ad67070e731428527fc6f9ce6af4c2ab53a7c
mmaksi <mmaksi.dev@gmail.com>
2024-02-29 12:36:50 +0100

    updated the common package to be published to npm

--- common/src/middleware/errors-handler.middleware.ts
+++ common/src/middleware/errors-handler.middleware.ts
@@ -12,6 +12,7 @@ export function errorsHandler(
       .status(error.statusCode)
       .json({ errors: error.serializeErrors() });
   }
+  console.log(error);
   return res
     .status(400)
     .json({ errors: [{ message: 'Something went wrong' }] });
