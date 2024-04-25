295284aadc13bf42228d87e4c56f3c040fe89af9
Mark Maksi <mmaksi.dev@gmail.com>
2024-04-25 17:29:22 +0200

    updated the logic to hash and compare user passwords

--- auth/src/services/password.ts
+++ auth/src/services/password.ts
@@ -1,23 +1,25 @@
-import bcrypt from 'bcryptjs';
+import { scrypt, randomBytes } from 'crypto';
+import { promisify } from 'util';
 import { BadRequestError } from '@maksimark/audiophile-common';
+
+const scryptAsync = promisify(scrypt);
 
 export class Password {
   static async toHash(password: string) {
+    const salt = randomBytes(8).toString('hex');
     try {
-      const hashedPassword = await bcrypt.hash(password, 10);
-      return hashedPassword;
+      const buf = (await scryptAsync(password, salt, 64)) as Buffer;
+      return `${buf.toString('hex')}.${salt}`;
     } catch (e) {
       throw new BadRequestError('Error hashing the password');
     }
   }
 
   static async compare(storedPassword: string, providedPassword: string) {
+    const [hashedPassword, salt] = storedPassword.split('.');
     try {
-      const hashedPassword = await bcrypt.compare(
-        providedPassword,
-        storedPassword
-      );
-      return hashedPassword;
+      const buf = (await scryptAsync(providedPassword, salt, 64)) as Buffer;
+      return buf.toString('hex') === hashedPassword;
     } catch (e) {
       throw new BadRequestError('Error hashing the password');
     }
